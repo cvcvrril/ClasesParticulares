fd4f43313d2981a0b023d783ea5e66cba694c621
ines (casa) <imartinezrodriguez@educa.madrid.org>
2025-03-01 23:44:38 +0100

    agregado el ejemplo del jdbc

--- psp/EjemploJdbc/src/main/java/ui/Ejemplo1.java
+++ psp/EjemploJdbc/src/main/java/ui/Ejemplo1.java
@@ -2,17 +2,14 @@ package ui;
 
 import jakarta.enterprise.inject.se.SeContainer;
 import jakarta.enterprise.inject.se.SeContainerInitializer;
-import services.ServiceEjemplos;
 
-public class Ejemplo3 {
+public class Ejemplo1 {
 
     public static void main(String[] args) {
         SeContainerInitializer seContainerInitializer = SeContainerInitializer.newInstance();
         final SeContainer container = seContainerInitializer.initialize();
-        ServiceEjemplos serviceEjemplos = container.select(ServiceEjemplos.class).get();
-        System.out.println("Ejemplo 3. Dame el nombre de una ciudad pasando el id");
-        System.out.println("");
-        System.out.println(serviceEjemplos.ejemplo3("01034"));
+
+
     }
 
 }
